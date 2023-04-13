6a1387805ca70c71aa4dbb8f47bdb31e7e7327c3
rylee <ryfrogquinn@gmail.com>
2023-04-13 11:14:58 -0500

    resized an image and changed the fonts

--- src/SinglePuppy.jsx
+++ src/SinglePuppy.jsx
@@ -18,12 +18,16 @@ export default function SinglePuppy() {
       {puppy && (
         <div class="single-player-view" key={puppy.id}>
           <div class="header-info">
-            <p class="pup-title">${puppy.name}</p>
-            <p class="pup-number">#${puppy.id}</p>
+            <p class="pup-title">{puppy.name}</p>
+            <p class="pup-number">#{puppy.id}</p>
           </div>
-          <p>Team: ${puppy.team ? puppy.team.name : "Unassigned"}</p>
-          <p>Breed: ${puppy.breed}</p>
-          <img src={puppy.imageUrl} alt="photo of the puppy" />
+          <p>Team: {puppy.team ? puppy.team.name : "Unassigned"}</p>
+          <p>Breed: {puppy.breed}</p>
+          <img
+            className="single-player-img"
+            src={puppy.imageUrl}
+            alt="photo of the puppy"
+          />
           <button id="see-all">Back to all players</button>
         </div>
       )}
